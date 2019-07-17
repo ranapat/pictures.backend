3d422e4dce3055d3a430a3ac1cbb0df60746a132
ranapat <ranapat@gmail.com>
2019-07-17 18:31:35 +0200

    watch

--- src/commands/watch/index.js
+++ src/commands/watch/index.js
@@ -1,5 +1,7 @@
-import exitHook from 'exit-hook';
+import fs from 'fs';
 import path from 'path';
+import { spawn } from 'child_process';
+import exitHook from 'exit-hook';
 import watch from 'node-watch';
 import { tf } from 'tasksf';
 import { icons, escapes } from 'clicomp';
@@ -13,6 +15,8 @@ let label;
 
 let modified = [];
 let deleted = [];
+
+let sequence;
 
 const clearScreen = () => {
   renderer = Renderer.instance;
@@ -41,16 +45,40 @@ const updateLabel = (value) => {
 
 const watchSingle = (directory, recursive) => {
   watch(directory, { recursive: recursive }, (event, name) => {
-    if (event == 'remove') {
+    if (event === 'remove') {
       deleted.push(name);
-    } else {
+
+      handleDeleted();
+    } else if (name && fs.existsSync(name) && fs.lstatSync(name).isFile()) {
       modified.push(name);
+
+      handleModified();
     }
-
-    console.log(path.dirname(name) + ' / ' + path.basename(name));
-
-    updateLabel(`Pictures in queue ${modified.length + deleted.length}`);
   });
+};
+
+const handleModified = () => {
+  sequence.push(tf.task((complete, self) => {
+    if (modified.length > 0) {
+      const picture = modified.pop();
+
+      updateLabel(`Files to process ${modified.length}, files to delete ${deleted.length}, updating...`);
+
+      spawn('./app', [ 'file', picture ]).on('exit', (code) => {
+        updateLabel(`Files to process ${modified.length}, files to delete ${deleted.length}, doing nothing...`);
+        complete();
+      });
+    }
+  }, 0));
+
+  if (!sequence.running) {
+    sequence.run();
+  }
+};
+
+const handleDeleted = () => {
+  updateLabel(`Files to process ${modified.length}, files to delete ${deleted.length}, doing nothing...`);
+
 };
 
 const init = (config, command, args, database, complete) => {
@@ -61,27 +89,18 @@ const init = (config, command, args, database, complete) => {
 
   clearScreen();
 
-
   exitHook(() => {
     renderer.cursorTo(0, 5);
     complete();
   });
 
+  sequence = tf.sequence();
+
   for (const directory of directories) {
     watchSingle(directory.path, directory.recursive);
   }
 
-  updateLabel(`Pictures in queue ${modified.length + deleted.length}`);
-
-  /*
-  const sequence = tf.sequence();
-
-  for (const picture of pictures) {
-    //sequence.push(task);
-  }
-
-  sequence.run();
-  */
+  updateLabel(`Files to process ${modified.length}, files to delete ${deleted.length}, doing nothing...`);
 };
 
 export { init };
